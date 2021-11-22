5a94fd886305c74cc769d8e3430ae7af0e581ab6
Jessica Gordon <ketchesj@gmail.com>
2021-11-22 16:45:46 +0000

    Step 5: Added functionality to top up an oystercard

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe Oystercard do
   it "shows users the balance" do
     expect(subject.balance).to eq 0
   end
+
+  describe "#top_up" do
+    it "adds money to my oystercard" do
+      oystercard = Oystercard.new
+      expect(oystercard.top_up(20)).to eq 20
+    end
+  end
 end
