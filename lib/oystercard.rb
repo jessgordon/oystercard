a08c2b082c535527a4abd0617340ea520ab42b15
Jessica Gordon <ketchesj@gmail.com>
2021-11-22 17:25:20 +0000

    Step 6: Added an exception when user tries to go over the limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,11 +3,19 @@
 class Oystercard
   attr_reader :balance
 
+  LIMIT = 90
+
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    raise "Cannot top-up #{amount}, goes over the limit of £#{LIMIT}" if limit?(amount)
+
     @balance += amount
+  end
+
+  def limit?(amount)
+    (amount + @balance) > LIMIT
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 RSpec.describe Oystercard do
+  let(:subject) { Oystercard.new }
+
   it { is_expected.to be_instance_of Oystercard }
 
   it "shows users the balance" do
@@ -9,8 +11,21 @@ RSpec.describe Oystercard do
 
   describe "#top_up" do
     it "adds money to my oystercard" do
-      oystercard = Oystercard.new
-      expect(oystercard.top_up(20)).to eq 20
+      expect(subject.top_up(20)).to eq 20
+    end
+
+    it "throws an exception if the new balance exceeds the limit" do
+      expect { subject.top_up(100) }.to raise_error "Cannot top-up 100, goes over the limit of £90"
+    end
+  end
+
+  describe "#limit?" do
+    it "verifies the balance is under the limit" do
+      expect(subject.limit?(10)).to eq false
+    end
+
+    it "verifies the balance is over the limit" do
+      expect(subject.limit?(100)).to eq true
     end
   end
 end
